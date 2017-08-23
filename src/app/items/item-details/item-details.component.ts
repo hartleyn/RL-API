571a6dce9caf5fcbfba48103abaef074a6d83404
Nicholas Hartley <nhartley18@me.com>
2017-08-23 02:02:46 -0400

    Implement all API endpoints and create angular web app

--- src/app/items/item-details/item-details.component.ts
+++ src/app/items/item-details/item-details.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { Item } from '../item';
 import { ItemService } from '../item.service';
 
